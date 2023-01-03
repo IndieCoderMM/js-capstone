eac29e940660cf2772bc25bda0e7f4078eeb7a4c
hein_thant <hthant00chk@gmail.com>
2023-01-03 14:42:49 +0630

    Fix linter errors

--- src/modules/get-characters.js
+++ src/modules/get-characters.js
@@ -3,7 +3,6 @@ const API_URL = 'https://www.amiiboapi.com/api/amiibo';
 const getCharacters = async () => {
   const response = await fetch(API_URL);
   const data = await response.json();
-  console.log(data);
   return data.amiibo;
 };
 
